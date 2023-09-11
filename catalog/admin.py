834975ea9f89bab922dfa6227df2dca270a2b5bb
Amor6 <135590534+Amor6@users.noreply.github.com>
2023-09-11 17:58:08 +0400

    DZ 3.4

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,13 +5,10 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product_name', 'price', 'category')
-    list_filter = ('category',)
+    list_display = ('id', 'product_name', 'price', 'category_catalog')
+    list_filter = ('category_catalog',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'category_name',)
-
-
-
--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
-    objects = None
     category_name = models.CharField(max_length=250, verbose_name='Категория продукта')
     information = models.TextField(max_length=250, verbose_name='Описание')
 
@@ -14,32 +13,20 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('category_name',)
-#
-
-
-
 
 class Product(models.Model):
     product_name = models.CharField(max_length=250, verbose_name='Наименование')
     information = models.TextField(max_length=250, verbose_name='Описание')
     imagef = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
+    category_catalog = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.PositiveIntegerField(verbose_name='Цена за покупку')
-    date_creation = models.DateTimeField(max_length=250, verbose_name='Дата создания')
-    date_change = models.DateTimeField(max_length=250, verbose_name='Дата последнего изменения')
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_change = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     def __str__(self):
-        return f'{self.product_name}'
+        return self.product_name
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ('product_name',)
-
-#
-#
-#
-
-
-
-
